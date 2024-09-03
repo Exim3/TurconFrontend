23f5476bf4949ba772ea17d94b7aa839feb63688
Exim3 <exim3@maatson.com>
2024-09-03 18:23:09 +0530

    s
    s

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // base: "/TurconClone/", // Uncomment this if you are deploying to a subpath on your domain
-  build: {
-    outDir: "client", // Specify the directory where the build output should go
-  },
+  // build: {
+  //   outDir: "client", // Specify the directory where the build output should go
+  // },
   plugins: [react()],
 });
