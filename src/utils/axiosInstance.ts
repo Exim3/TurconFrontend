191073fcc80c821285a3269e14db8d32baf24470
Exim3 <exim3@maatson.com>
2024-09-03 18:43:09 +0530

    axios

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://turcon-frontend.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
